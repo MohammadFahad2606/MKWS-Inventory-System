3be5ba832941360dd8668e2d79cec649a18d34b4
MohammadFahad2606 <design4youonline@gmail.com>
2025-08-16 22:41:50 +0500

    routes and product page and footer edit

--- client/src/widgets/layout/footer.jsx
+++ client/src/widgets/layout/footer.jsx
@@ -8,17 +8,18 @@ export function Footer({ brandName, brandLink, routes }) {
   return (
     <footer className="py-2">
       <div className="flex w-full flex-wrap items-center justify-center gap-6 px-2 md:justify-between">
-        <Typography variant="small" className="font-normal text-inherit">
+        <Typography variant="small" className="font-normal text-inherit text-center md:text-left">
           &copy; {year}, made with{" "}
           <HeartIcon className="-mt-0.5 inline-block h-3.5 w-3.5 text-red-600" /> by{" "}
           <a
             href={brandLink}
             target="_blank"
+            rel="noopener noreferrer"
             className="transition-colors hover:text-blue-500 font-bold"
           >
             {brandName}
           </a>{" "}
-          for a better web.
+          for Muslim Khatri Medical & Diagnostic Center.
         </Typography>
         <ul className="flex items-center gap-4">
           {routes.map(({ name, path }) => (
@@ -26,7 +27,7 @@ export function Footer({ brandName, brandLink, routes }) {
               <Typography
                 as="a"
                 href={path}
-                target="_blank"
+                rel="noopener noreferrer"
                 variant="small"
                 className="py-0.5 px-1 font-normal text-inherit transition-colors hover:text-blue-500"
               >
@@ -41,13 +42,13 @@ export function Footer({ brandName, brandLink, routes }) {
 }
 
 Footer.defaultProps = {
-  brandName: "Creative Tim",
-  brandLink: "https://www.creative-tim.com",
+  brandName: "Mohammad Fahad",
+  brandLink: "https://mfahaddev.netlify.app/", 
   routes: [
-    { name: "Creative Tim", path: "https://www.creative-tim.com" },
-    { name: "About Us", path: "https://www.creative-tim.com/presentation" },
-    { name: "Blog", path: "https://www.creative-tim.com/blog" },
-    { name: "License", path: "https://www.creative-tim.com/license" },
+    { name: "Home", path: "/dashboard/home" },
+    { name: "Products", path: "/dashboard/product" },
+    { name: "Tables", path: "/dashboard/tables" },
+    { name: "Notifications", path: "/dashboard/notifications" },
   ],
 };
 
